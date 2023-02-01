08670705b4d786f6d9732ca57130c4b64418a36a
Masum Hasan <masumhasan92@gmail.com>
2023-02-02 01:49:52 +0600

    iko

--- c3/2string_slicing.py
+++ c3/2string_slicing.py
@@ -1,4 +1,4 @@
-hello = "Whatsupp"
+hello = "Whatsup"
 # name = input("Enter Name: ")
 # c = name+hello
 # print(c)
@@ -6,3 +6,6 @@ print(hello[0])
 x = 3
 d = "345634"
 print(d[x])
+# hello[3]= "r" ====> Does not work
+print(hello[1:4])  # slicing the string from index 1to3   0<1---3<4
+print(hello[-1])  # print the last index with -1
