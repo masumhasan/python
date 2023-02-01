064ec147dd0f0db915ccf1acf45579b1e62688c0
Masum Hasan <masumhasan92@gmail.com>
2023-02-01 23:41:31 +0600

    u

--- c2/1variables.py
+++ c2/1variables.py
@@ -4,6 +4,17 @@ a = 'masum'
 b = 203
 
 c = 7.5
+d = True  # True or False only
+e = None  # To return none
 print(a)
 print(b)
 print(c)
+print(d)
+print(e)
+
+# print type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
---
+++ c2/2operators.py
@@ -0,0 +1,21 @@
+a = 3
+b = 6
+print("value of a+b is", 3+4)
+print("value of a+b is", a+b)
+print("value of a-b is", a-b)
+print("value of a/b is", a/b)
+print("value of a*b is", a*b)
+
+
+# Assignment Operators
+
+x = 34
+print(x)
+x += 2  # (x=x+2)
+print(x)
+x -= 2  # (x=x-2)
+print(x)
+x *= 2  # (x=x*2)
+print(x)
+x /= 2  # (x=x/2) This coverts the value in float
+print(x)
