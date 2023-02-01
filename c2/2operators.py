8cfdb5ed57438360aa75afe03415407cbce5bdd4
Masum Hasan <masumhasan92@gmail.com>
2023-02-02 00:19:15 +0600

    c

--- c2/2operators.py
+++ c2/2operators.py
@@ -8,7 +8,7 @@ print("value of a*b is", a*b)
 
 
 # Assignment Operators
-
+# Value update
 x = 34
 print(x)
 x += 2  # (x=x+2)
@@ -19,3 +19,18 @@ x *= 2  # (x=x*2)
 print(x)
 x /= 2  # (x=x/2) This coverts the value in float
 print(x)
+
+
+# Comparisn Operaator
+
+# y= (33>5)
+# y= (33<5)
+# y= (33>=5)
+# y= (33<5)
+# y= (33==5)
+# y= (33!=5)
+y = (33 > 5)
+print(y)
+
+
+# Logical Operators
