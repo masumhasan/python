33aa866af9cc43e68961cf779abcf6ede4fce445
Masum Hasan <masumhasan92@gmail.com>
2023-02-02 05:26:37 +0600

    p

---
+++ c4/7prmarks.py
@@ -0,0 +1,9 @@
+m1 = int(input("Enter Marks For Student Number 1:"))
+m2 = int(input("Enter Marks For Student Number 2:"))
+m3 = int(input("Enter Marks For Student Number 3:"))
+m4 = int(input("Enter Marks For Student Number 4:"))
+m5 = int(input("Enter Marks For Student Number 5:"))
+m6 = int(input("Enter Marks For Student Number 6:"))
+markList = [m1, m2, m3, m4, m5, m6]
+markList.sort()
+print(markList)
---
+++ c4/9sum_list.py
@@ -0,0 +1,9 @@
+# pr4
+a = [3, 7, 9, 4]
+s = a[0]+a[1]+a[2]+a[3]
+print(s)
+print(sum(a))  # same
+
+# pr5
+t = (7, 0, 8, 0, 0, 9)
+print(t.count(0))
