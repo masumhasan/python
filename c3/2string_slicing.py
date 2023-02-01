03bdf1598980da59177c968b21846b7543b9f4b1
Masum Hasan <masumhasan92@gmail.com>
2023-02-02 02:15:43 +0600

    u

--- c3/2string_slicing.py
+++ c3/2string_slicing.py
@@ -7,5 +7,12 @@ x = 3
 d = "345634"
 print(d[x])
 # hello[3]= "r" ====> Does not work
-print(hello[1:4])  # slicing the string from index 1to3   0<1---3<4
+print(hello[0:4])
+# ([:4 same as] use 1:4 to choose starting index) slicing the string from index 1to3   0<1---3<4
+print(hello[3:])
 print(hello[-1])  # print the last index with -1
+
+
+# SKIP CHAR
+abc = "abcdefghijklmnopqrstuvvxyz"
+print(abc[0:-1:2])  # print every second char
