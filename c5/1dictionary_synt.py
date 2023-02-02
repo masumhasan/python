1c6aca2a3390b5f093f7ef416c22ec28f139f39a
Masum Hasan <masumhasan92@gmail.com>
2023-02-02 06:04:18 +0600

    p

---
+++ c5/1dictionary_synt.py
@@ -0,0 +1,10 @@
+firstDictionary = {
+    "Name": "Nur Hasan Masum",
+    "Email": "masumhasan92@gmail.com",
+    "Marks": [5.0, 4.44, 3.8],
+    "newDick": {"address": "Thakurgaon"}
+}
+print(firstDictionary["Name"])
+print(firstDictionary["Email"])
+print(firstDictionary["Marks"])
+print(firstDictionary['newDick']['address'])
