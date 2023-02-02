ddf05400442e6d28e495193c21ef382e929fa560
Masum Hasan <masumhasan92@gmail.com>
2023-02-02 06:07:03 +0600

    p

--- c5/1dictionary_synt.py
+++ c5/1dictionary_synt.py
@@ -4,7 +4,8 @@ firstDictionary = {
     "Marks": [5.0, 4.44, 3.8],
     "newDick": {"address": "Thakurgaon"}
 }
-print(firstDictionary["Name"])
+print(firstDictionary["Name"])  # access a dictionary element by key
 print(firstDictionary["Email"])
 print(firstDictionary["Marks"])
+# access element of a dictionary inside another dictionary
 print(firstDictionary['newDick']['address'])
